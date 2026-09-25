cc477374e28804cd1a8316d27cdfb031cfcd2bf1
assassinor <7605023+assassinor@users.noreply.github.com>
2026-09-25 18:56:44 +0800

    minitools 2026.09.25.2 (#28)
    Co-authored-by: runner <runner@sjc20-cw761-bdd63bb2-dbf0-4475-9309-aaa35356fb38-129C98ADA149.local>

--- Casks/minitools.rb
+++ Casks/minitools.rb
@@ -1,6 +1,6 @@
 cask "minitools" do
-  version "2026.09.25.1"
-  sha256 "84b8c3ff96bbae308585223a5771e3434cfbae99e33151727521c92ef61ec426"
+  version "2026.09.25.2"
+  sha256 "7463c437a014af366dd083592006e7ef7b0a7a0458cd9e06a30d63e1078e3117"
 
   url "https://github.com/oh-my-brew/miniTools/releases/download/v#{version}/miniTools-#{version}.zip"
   name "miniTools"
